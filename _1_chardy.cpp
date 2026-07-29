b7705145a75ee878c5b277d5de849603eae2e7d7
harshil <harshilvaddoriya096@gmail.com>
2026-07-30 03:23:34 +0530

    charecter array info.

---
+++ _1_chardy.cpp
@@ -0,0 +1,59 @@
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+
+// char ch='p';
+// int position=ch-'a';
+// cout<<position;
+
+// char name[]={'a','b','c'};
+// cout<<name;
+
+// char name[]={'a','b','c','\0'};
+// cout<<name;
+
+// char name[]={'a','b','c','\0'};
+// cout<<name;
+
+//note;-
+//char name[30]={'a','b','c'};
+//cout<<name;
+
+
+// char name[]="harshil";
+// cout<<name;
+
+
+// char name[50]="harshil vadadoriya";
+// cout<<name;
+
+// char name[50]="harshil vadadoriya";
+// cout<< strlen(name)<<'\n';
+// cout << sizeof(name);  // Output: 50
+
+
+// char name[50];
+// cout<<"enter your name:";
+// cin>>name;
+// cout<<name<<endl;   //its ignor string after whitespace
+// cout<< strlen(name)<<'\n';
+// cout<<strlen(name);
+
+
+// char name[50];
+// cout<<"enter your  name:";
+// cin.getline(name,50,'*');
+
+// cout<<"your name is :"<<name<<'\n'; 
+
+
+
+}
+
+
+
+
+
